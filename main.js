71983e3e2b90c34fae71c890bca16194c5307e2c
EikaFrank <150336508+EikaFrank@users.noreply.github.com>
2024-02-12 15:02:24 +0100

    startGame function
    Not yet able to hide the fighterSelectionForm when fighter've been choosen

--- main.js
+++ main.js
@@ -2,9 +2,39 @@ import {Cactus}  from "./module/cactus.js";
 import { Fighter } from "./module/fighter.js";
 import {Nettle}  from "./module/nettle.js";
 
+<<<<<<< Updated upstream
 const c = new Cactus(500);
 console.log(c)
 c.attackHealth(10)
+=======
+
+//Start game function and hide fighterSelection form when user've choose fighter 
+
+document.addEventListener("DOMContentLoaded", function(){
+    const fighterSelectionForm = document.getElementById("fighterSelection");
+
+    const cactusBtn = document.getElementById('cactusBtn');
+    const nettleBtn = document.getElementById('nettleBtn');
+    
+    cactusBtn.addEventListener('click', function(){
+        fighterSelectionForm.classList.add('hide');
+        startGame('cactus');
+    });
+    
+    nettleBtn.addEventListener('click', function(){
+        fighterSelectionForm.classList.add('hide');
+        startGame('nettle');
+    })
+    
+    function startGame(selectedPlant){
+        console.log ("You choose the " + selectedPlant + " and the game begin!");
+    }
+    
+    });
+
+
+const c = new Cactus(10);
+>>>>>>> Stashed changes
 console.log(c)
 c.needle(10)
 
