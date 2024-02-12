5bcb376800ada6759d73573efc87b098b6093196
johannaostling <johanna.ostling@gritacademy.se>
2024-02-12 13:48:01 +0100

    Setting up for solution

--- main.js
+++ main.js
@@ -2,12 +2,12 @@ import {Cactus}  from "./module/cactus.js";
 import { Fighter } from "./module/fighter.js";
 import {Nettle}  from "./module/nettle.js";
 
-const c = new Cactus(10);
+const c = new Cactus(500);
 console.log(c)
-// c.attackHealth(10)
-
+c.attackHealth(10)
+console.log(c)
 c.needle(10)
-console.log(c)
 
-const c2 = new Cactus(8000);
-console.log(c2)
+
+c.attackHealth(c.needle())
+console.log(c)
