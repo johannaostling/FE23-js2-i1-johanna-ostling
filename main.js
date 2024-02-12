6772c496c0634b11d7564970c9b3f50c90d7feb6
EikaFrank <150336508+EikaFrank@users.noreply.github.com>
2024-02-12 15:16:38 +0100

    2 correction after the commit

--- main.js
+++ main.js
@@ -1,37 +1,38 @@
-import {Cactus}  from "./module/cactus.js";
+import { Cactus } from "./module/cactus.js";
 import { Fighter } from "./module/fighter.js";
-import {Nettle}  from "./module/nettle.js";
+import { Nettle } from "./module/nettle.js";
+
+
+
+//Start game function and hide fighterSelection form when user've choose fighter 
+
+document.addEventListener("DOMContentLoaded", function () {
+    const fighterSelectionForm = document.getElementById("fighterSelection");
+
+    const cactusBtn = document.getElementById('cactusBtn');
+    const nettleBtn = document.getElementById('nettleBtn');
+
+    cactusBtn.addEventListener('click', function () {
+        fighterSelectionForm.classList.add('hide');
+        startGame('cactus');
+    });
+
+    nettleBtn.addEventListener('click', function () {
+        fighterSelectionForm.classList.add('hide');
+        startGame('nettle');
+    })
+
+    function startGame(selectedPlant) {
+        console.log("You choose the " + selectedPlant + " and the game begin!");
+    }
+
+});
 
 const c = new Cactus(500);
 console.log(c)
 c.attackHealth(10)
 
-//Start game function and hide fighterSelection form when user've choose fighter 
 
-document.addEventListener("DOMContentLoaded", function(){
-    const fighterSelectionForm = document.getElementById("fighterSelection");
-
-    const cactusBtn = document.getElementById('cactusBtn');
-    const nettleBtn = document.getElementById('nettleBtn');
-    
-    cactusBtn.addEventListener('click', function(){
-        fighterSelectionForm.classList.add('hide');
-        startGame('cactus');
-    });
-    
-    nettleBtn.addEventListener('click', function(){
-        fighterSelectionForm.classList.add('hide');
-        startGame('nettle');
-    })
-    
-    function startGame(selectedPlant){
-        console.log ("You choose the " + selectedPlant + " and the game begin!");
-    }
-    
-    });
-
-
-const c = new Cactus(10);
 console.log(c)
 c.needle(10)
 
